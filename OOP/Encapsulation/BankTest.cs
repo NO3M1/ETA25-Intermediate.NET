5c6973e04d567ac7399a66a3a1a8184a0e69a761
ENDAVA\Nszoke <szokenoemi@yahoo.com>
2025-05-21 15:55:16 +0300

    Encapsulation

---
+++ OOP/Encapsulation/BankTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ETA25_Intermediate_C_.OOP.Encapsulation
+{
+    public class BankTest
+    {
+
+        private Wallet wallet;
+
+
+        [SetUp]
+        public void Setup()
+        {
+          wallet = new Wallet(1000);  
+        }
+
+        [Test]
+        public void TestDeposit()
+        {
+            wallet.Deposit(500);
+            //Console.WriteLine(wallet.GetBalance());
+            Console.WriteLine(wallet.Balance);
+      
+        }
+
+        [Test]
+        public void TestWithdraw()
+        {
+            wallet.Withdraw(200);
+            //Console.WriteLine(wallet.GetBalance());
+        }
+
+
+
+
+    }
+}
---
+++ OOP/Encapsulation/Wallet.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETA25_Intermediate_C_.OOP.Encapsulation
+{
+    public class Wallet
+    {
+        private decimal balance;
+      
+        public Wallet(decimal initialAmonunt)
+        {
+            this.balance = initialAmonunt;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+            }
+            else
+            {
+                Console.WriteLine("Deposit amount must be positive");
+            }
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount > 0 && amount <= balance)
+            {
+                balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Invalid withdrawal amount");
+            }
+        }
+
+  /*      public decimal GetBalance() 
+        { 
+            return balance; 
+        }*/
+
+        public decimal Balance
+        {
+            get
+            {
+                return balance;
+            }
+
+        }
+
+
+    }
+}
